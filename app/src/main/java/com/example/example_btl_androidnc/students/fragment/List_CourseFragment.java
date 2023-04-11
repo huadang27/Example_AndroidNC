c9c803fa4f5a689a6d8f36f207e3e3cb12af5a9b
tien01nx <114860203+tien01nx@users.noreply.github.com>
2023-04-11 10:10:28 +0700

    thêm chức năng tìm kiếm ở danh sách lớp học

--- app/src/main/java/com/example/example_btl_androidnc/students/fragment/List_CourseFragment.java
+++ app/src/main/java/com/example/example_btl_androidnc/students/fragment/List_CourseFragment.java
@@ -7,10 +7,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.example_btl_androidnc.R;
 import com.example.example_btl_androidnc.students.adapter.ListCourseAdapter;
@@ -34,6 +36,7 @@ public class List_CourseFragment extends Fragment {
     private RecyclerView recyclerView;
     private List<UserCourse> userCourses;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private SearchView  searchView;
 
     public List_CourseFragment() {
 
@@ -52,6 +55,7 @@ public class List_CourseFragment extends Fragment {
         userCourses = new ArrayList<>();
         recyclerView = view.findViewById(R.id.recyclerview);
         swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout);
+        searchView = view.findViewById(R.id.searchView);
         mySharedPreferences = new MySharedPreferences(getContext());
 
         fetchData();
@@ -60,6 +64,23 @@ public class List_CourseFragment extends Fragment {
             @Override
             public void onRefresh() {
                 fetchData();
+            }
+        });
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                // bắt sự kiện khi ấn enter
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // bắt luôn sự kiện
+                filterCourses(query);
+                return false;
             }
         });
 
@@ -76,6 +97,7 @@ public class List_CourseFragment extends Fragment {
                     Log.d("test", "Response code: " + response.code());
                     Log.d("test", "Response message: " + response.message());
                 }
+                userCourses = response.body();
                 List<UserCourse> courseList = response.body();
                 List<UserCourse> filteredCourseList = new ArrayList<>();
                 for (UserCourse course : courseList) {
@@ -99,6 +121,16 @@ public class List_CourseFragment extends Fragment {
         ListCourseAdapter adapter = new ListCourseAdapter(getContext(), movieList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+    }
+
+    private void filterCourses(String query) {
+        List<UserCourse> filteredCourseList = new ArrayList<>();
+        for (UserCourse course : userCourses) {
+            if (course.getStatus() == 1 && course.getName().toLowerCase().contains(query.toLowerCase())) {
+                filteredCourseList.add(course);
+            }
+        }
+        PutDataIntoRecyclerView(filteredCourseList);
     }
 
 
